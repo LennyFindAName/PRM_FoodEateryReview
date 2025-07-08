eb23f5098d3230328c147bba8b2d51b6b517b297
Halocau <buitienquat2k3@gmail.com>
2025-07-08 21:06:58 +0700

    fix: navigate through homeactivity

--- app/src/main/java/prm392/project/view/LoginActivity.java
+++ app/src/main/java/prm392/project/view/LoginActivity.java
@@ -85,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
 
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(intent);
                     finish();
                 } else {
