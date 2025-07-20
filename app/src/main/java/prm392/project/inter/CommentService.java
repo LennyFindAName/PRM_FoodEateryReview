541f7d0a57cee6d5f2d5966d8bd7cf1ceaaedd97
dangnh <dangnhhe186256@fpt.edu.vn>
2025-07-20 16:24:20 +0700

    Update for elements

--- app/src/main/java/prm392/project/inter/CommentService.java
+++ app/src/main/java/prm392/project/inter/CommentService.java
@@ -1,4 +1,21 @@
 package prm392.project.inter;
 
-public class CommentService {
+import java.util.List;
+
+import prm392.project.model.Comment;
+import prm392.project.model.DTOs.CommentRequest;
+import prm392.project.model.DTOs.CommentResponse;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface CommentService {
+    @POST("Comment")
+    Call<Void> postComment(@Body CommentRequest commentRequest);
+
+    @GET("Comment/blog/{blogId}")
+    Call<List<CommentResponse>> getComments(@Path("blogId") int blogId);
+
 }
--- app/src/main/java/prm392/project/model/Comment.java
+++ app/src/main/java/prm392/project/model/Comment.java
@@ -1,4 +1,78 @@
 package prm392.project.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 public class Comment {
+
+    @SerializedName("commentId")
+    private int commentId;
+
+    @SerializedName("content")
+    private String content;
+
+    @SerializedName("commentDate")
+    private String commentDate; // Use String for compatibility with ISO DateTime
+
+    @SerializedName("commentLike")
+    private Integer commentLike;
+
+    @SerializedName("userId")
+    private Integer userId;
+
+    @SerializedName("username")
+    private String username; // From c.User?.DisplayName
+
+    // Empty constructor
+    public Comment() {}
+
+    // Getters and Setters
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(String commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public Integer getCommentLike() {
+        return commentLike;
+    }
+
+    public void setCommentLike(Integer commentLike) {
+        this.commentLike = commentLike;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
